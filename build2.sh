b8c5680d22f8d3473fbba0bd9f82ec9e2eb3d719
Janus <ysangkok@gmail.com>
2015-04-06 02:00:40 +0200

    move fsroot out of v86

--- build2.sh
+++ build2.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -ex
-rm -rf /var/www/v86/fsroot
-cp -fr filledinitramfs/initramfs/. /var/www/v86/fsroot/
-cat > /var/www/v86/fsroot/etc/fstab <<END
+rm -rf ../fsroot
+cp -fr filledinitramfs/initramfs/. ../fsroot/
+cat > ../fsroot/etc/fstab <<END
 proc            /proc      proc     defaults     0      0
 devtmpfs        /dev       devtmpfs defaults     0      0
 sysfs           /sys       sysfs    defaults     0      0
 END
-cat > /var/www/v86/fsroot/etc/rcS <<END
+cat > ../fsroot/etc/rcS <<END
 #!/bin/bash -ex
 /busybox-i486 mount -a
 END
-chmod +x /var/www/v86/fsroot/etc/rcS
-cat > /var/www/v86/fsroot/etc/inittab <<END
+chmod +x ../fsroot/etc/rcS
+cat > ../fsroot/etc/inittab <<END
 ::sysinit:/etc/rcS
 ::respawn:-/bin/bash
 #ttyS1::respawn:-login -f root
@@ -23,9 +23,9 @@ cat > /var/www/v86/fsroot/etc/inittab <<END
 ::ctrlaltdel:/sbin/reboot
 ::shutdown:/etc/shutdown
 END
-cat > /var/www/v86/fsroot/init <<END
+cat > ../fsroot/init <<END
 #!/bin/bash -ex
 #exec oneit -p /bin/bash
 exec /busybox-i486 init
 END
-python /var/www/v86/fs2json.py /var/www/v86/fsroot --out ../fs.json
+python /var/www/v86/fs2json.py ../fsroot --out ../fs.json
